600fa0097424e1304f005f9bd6da700595494735
cocoalix <cocoalix@gmail.com>
2021-12-05 03:42:07 +0900

    implement HTTPClient() with HTTP/3

--- CurlIM/Program.cs
+++ CurlIM/Program.cs
@@ -1,3 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
+//var message = new HttpRequestMessage();
+//message.Version = Version.Parse("3.0");
+
+var client = new HttpClient();
+client.DefaultRequestVersion = Version.Parse("3.0");
+
+var response = await client.GetAsync("https://localhost:5001");
+var html = await response.Content.ReadAsStringAsync();
+
+Console.WriteLine(html);
